e9df5ade084017d3c2bcb6e254330e3e5f33634a
NesanduGMS <sithnukagunarathna@gmail.com>
2024-10-12 12:21:35 +0530

    Server created

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Nesa');
+});
+
+app.listen(3005, () => {
+    console.log('Server is running on port 3005');
+});

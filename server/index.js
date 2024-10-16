74600c308bdb782524e1995e988136fe55993ab8
LAHITUVISH01 <vishmithahiru68@gmail.com>
2024-10-16 22:05:10 +0530

    db connected and data base inserted in to index

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { default: db } = require('./utils/db');
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send('Hello Nesa');
---
+++ server/utils/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+
+// Create a connection pool to the database
+
+const db = mysql.createPool({
+    host: "db-mysql-nyc3-03427-do-user-17948696-0.l.db.ondigitalocean.com",
+    user: "doadmin",
+    password: "AVNS_2wkHuRrvaIqCM21kN2q", // Replace with your MySQL password
+    database: "defaultdb",
+    port: 25060
+  });
+  
+  
+  // Ensure MySQL connection works
+  db.getConnection((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+    } else {
+      console.log('Connected to MySQL');
+    }
+  });
+
+  module.exports = db;
